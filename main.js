d53f43ae1a86496ffbe100afd788ec83aea0c919
azufb <curiousazunas@gmail.com>
2021-09-30 17:18:09 +0900

    2-4

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    // dataオプションに定義したプロパティの値が、HTML内の{{プロパティ}}部分に入る。
+    products: [
+      {code: 'A01', name: 'プロダクトA'},
+      {code: 'B01', name: 'プロダクトB'},
+      {code: 'C01', name: 'プロダクトC'}
+    ]
   }
 });
