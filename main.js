ef8818d836f806ef0461d0512385508d98eac8b8
azufb <curiousazunas@gmail.com>
2021-10-02 19:38:14 +0900

    2-7~2-10

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ var app = new Vue({
   el: '#app',
   data: {
     // dataオプションに定義したプロパティの値が、HTML内の{{プロパティ}}部分に入る。
-    products: [
-      {code: 'A01', name: 'プロダクトA'},
-      {code: 'B01', name: 'プロダクトB'},
-      {code: 'C01', name: 'プロダクトC'}
-    ]
+    message: 'これは一方通行のバインドです。',
+  },
+  methods: {
+    
   }
 });
